e6ab1184a3b0ba6c87049dce1786ceccc5cf095f
giladby4 <giladby4@gmail.com>
2025-12-06 20:55:44 +0200

    Adding answers of parts 1-2

--- hw1/answers.py
+++ hw1/answers.py
@@ -9,70 +9,45 @@ math (delimited with $$).
 # Part 1 answers
 
 part1_q1 = r"""
-**Your answer:**
+
+1. False. Some splits can represent the original distribution better then others, and that can make them more usefull. 
+In addition, some splits can be extremly unuseful because of imbalance, for example if a class dont apear in the train but only in the test, or there may be edge cases that apear only in the test. 
 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+2. False. The test-set must remain untuched in the training proccess, because using it may reduce the test affecienity and also cause overfitting.
 
+
+3. True. In the cross validation proccess we average all the validation-set performence so the error is based on several trains and validation splits instead of on one.
+
+4. True. Injecting noises can help to test if the model is overfitting, because it make the data more random and prevent memorizing patterns.
 """
 
 part1_q2 = r"""
-**Your answer:**
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+No. As we answered in q1-2, the test must remain untuched during the training - or the all train proccess will be biased by the test-set.
 """
 
 # ==============
 # Part 2 answers
 
 part2_q1 = r"""
-**Your answer:**
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+$\Delta$ is representing the margin, so when $\Delta < 0$, the margin we allow is negetive, what mean that we 'tolerate' misclassify as far as they are small enough (smaller the $\Delta$).
+In other words, the model will incur zero loss even in cases of misclassify. 
 
 """
-
 part2_q2 = r"""
-**Your answer:**
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+The model learn the 'average digit shape', giving score to each pixel based on the probability it is part of the digit. 
+Errors could coure because of rotation of a digit; bigger or smaller, thiner or thicker digit than expected;  a digit written in iregular way; or, two similiar templates.
+For example, there is a 6 that the model 'think' is 2, maybe because it rotated. also, a 4,5 and 7 written in an irregilar way that tagged wrongly. 
 
 """
 
 part2_q3 = r"""
-**Your answer:**
+learning rate is good. if it was too high, we could see the graph going down very fast, and then stay the same. if it was too low, we could see the graph going down slowly and not get the minimal loss. in our grapg, the graph go down fast but keep getting down all the way.
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+The model is slightly overfitted to the training set. we can see the accuracy rate of the training is bettet then the test accuracy, but not by much and the test accuracy is high itself. 
 
 """
 
